ee7d2f4b5bb67e3ef01c389dc402e6e319473fcf
Jacob Mousser <89864244+CornOnTheJacob@users.noreply.github.com>
2022-11-03 12:45:42 -0700

    Final Build

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Jacob Mousser
+ * Computer Programming and Mobile App Design
+ * 11/3/2022
+ */
+using System;
+using System.Text;
+
+namespace ConvertHex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Checked string
+            string input = "hello world";
+
+            //Run function
+            string hexadecimal = ConvertToHex(input);
+
+            //Print output
+            Console.WriteLine(hexadecimal);
+        }
+
+        static string ConvertToHex(string input)
+        {
+            //Variable setup
+            string output = "";
+
+            //Convert to hexadecimal
+            byte[] bytes = Encoding.UTF8.GetBytes(input);
+            foreach (byte b in bytes)
+            {
+                output += b.ToString("x") + " ";
+            }
+
+            //Return
+            return output;
+        }
+    }
+}
